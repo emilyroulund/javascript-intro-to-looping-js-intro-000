85d072534eb7983b60715d32fc4f26192e28f668
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-20 00:56:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,17 +1,20 @@
-function forLoop(array){
-  for (let i = 0; i<25; i ++){
-    if (i === 0){
-   var n = "I am 1 strange loop.";
-    }
-    else { 
-   n = `I am ${i} strange loops.`;
-    }
-return [...array, n];
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`);
+     } else {
+       array.push(`I am ${i} strange loops.`);
+     }
   }
+  return array;
 }
 
 function whileLoop(n){
-  
+  while (n>0){
+    n--; 
+    return n;
+  }
+  return 'done'
 }
 
 function doWhileLoop(num){
